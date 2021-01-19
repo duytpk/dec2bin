f278ceaa2198c2162b896d09f44fb83aeae521d2
duy.tranphuongkhang@haravan.com <duy.tranphuongkhang@haravan.com>
2021-01-19 17:36:55 +0700

    Upload dec2bin

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func checknum() {
+	var num int64
+	for {
+		fmt.Print("Enter Decimal Number:")
+		fmt.Scanln(&num)
+		output := strconv.FormatInt(num, 2)
+		fmt.Print("Output: ", output)
+		os.Exit(0)
+	}
+
+}
+
+func main() {
+	for {
+		fmt.Println("Press 1 to run")
+		fmt.Println("Press 2 to exit")
+		var input int
+		fmt.Scanln(&input)
+		if input < 1 {
+			fmt.Println("invalid input")
+		} else if input > 2 {
+			fmt.Println("invalid input")
+		} else if input == 1 {
+			checknum()
+		} else {
+			break
+		}
+	}
+
+}
